f3e86785dd1fac8038d5d5f238b677f8967e15b9
none <none@none.com>
2023-04-20 12:55:03 +0900

    feat: add birthday alert

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const Schedule = require("node-schedule");
 const hookurl = process.env.HOOK_URL;
 const mattermost = new Mattermost(hookurl);
 
+/**
+ * 입퇴실
+ */
 const presentRule = new Schedule.RecurrenceRule();
 presentRule.tz = 'Asia/Seoul';
 presentRule.dayOfWeek = [1, 2, 3, 4, 5];
@@ -32,4 +35,36 @@ const leaveJob = Schedule.scheduleJob(leaveRule, function () {
     username: "퇴실체크 봇",
   });
 });
+
+/**
+ * 생일
+ */
+const xlsxFile = require('read-excel-file/node');
+let birthdayJobs = [];
+
+xlsxFile("./ssafy_09_09_birthday_list.xlsx").then((members) => {
+  for (let idx = 0; idx < members.length; idx++) {
+    const name = members[idx][0];
+    let date = members[idx][1];
+    date = date.split(' ');
+
+    birthdayJobs.push(Schedule.scheduleJob(`0 0 0 ${date[1]} ${date[0]} *`, function () {
+      mattermost.send({
+        text: `#### ✨오늘은 ${name}의 생일입니다✨\n다들 축하해주세요^^`,
+        channel: "off-topic",
+        username: "생일축하 봇",
+      })
+    }));
+  }
+
+  // test
+  birthdayJobs.push(Schedule.scheduleJob(`0 0 13 20 4 *`, function () {
+    mattermost.send({
+      text: `#### ✨오늘은 테스트의 생일입니다✨\n다들 축하해주세요^^`,
+      channel: "off-topic",
+      username: "생일축하 봇",
+    })
+  }));
+});
+
 
